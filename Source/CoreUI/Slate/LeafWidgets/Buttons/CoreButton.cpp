1a022961c54691147958622ce7f0e3698a0de5aa
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-07 23:36:11 +0530

    Added  CoreButton  Slate  Class

---
+++ Source/CoreUI/Slate/LeafWidgets/Buttons/CoreButton.cpp
@@ -0,0 +1,65 @@
+﻿#include "CoreButton.h"
+
+#include "Slate/DeferredCleanupSlateBrush.h"
+#include "Styling/SlateBrush.h"
+
+
+void SCoreButton::Construct(const FArguments& InArgs)
+{
+	ButtonText = InArgs._ButtonText ;
+	OnClicked = InArgs._OnClicked;
+	ButtonImage = Convert_UT2D_SlateBrush(InArgs._ButtonImage);
+	
+	ChildSlot
+	[    SNew(SVerticalBox)
+		+ SVerticalBox::Slot()
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Center)
+			.Padding(5.0f)
+		
+			[
+				SNew(STextBlock)
+					.Text(FText::FromString(ButtonText))
+			]
+
+		+ SVerticalBox::Slot()
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Center)
+			.Padding(5.0f)
+			[
+				SNew(SImage)
+					.Image(ButtonImage.Get())
+			]
+	];
+}
+
+void SCoreButton::SetText( FString& NewText)
+{
+	ButtonText = NewText;
+	if (TextBlock.IsValid())
+	{
+		TextBlock->SetText(FText::FromString(NewText));
+	}
+}
+
+void SCoreButton::SetImage( UTexture2D* NewImage)
+{
+	ButtonImage  =  Convert_UT2D_SlateBrush(NewImage);
+	if (NewImage != nullptr)
+	{
+		if (ImageWidget.IsValid())
+		{
+			ImageWidget->SetImage(ButtonImage.Get());
+		}
+	}
+}
+
+TSharedPtr<FSlateBrush> SCoreButton::Convert_UT2D_SlateBrush(UTexture2D* NewImage)
+{
+	FSlateBrush* Brush = new FSlateBrush();
+	Brush->SetResourceObject(NewImage);
+	TSharedPtr<FSlateBrush>  Image  =MakeShareable(Brush);
+	return Image;
+}
+
+
---
+++ Source/CoreUI/Slate/LeafWidgets/Buttons/CoreButton.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "Widgets/Input/SButton.h"
+#include "Slate/DeferredCleanupSlateBrush.h"
+
+/**
+ * this is a custom button class inheriting from SButton class
+ * the macro SLATE_BEGIN_ARGS() starts the button declaration'
+ * 
+ *
+ * 
+ */
+class SCoreButton : public SButton
+{
+public:
+	SLATE_BEGIN_ARGS(SCoreButton)
+		: _ButtonText(TEXT("Button"))
+	{}
+ 
+	SLATE_ARGUMENT(FString, ButtonText)
+	SLATE_ARGUMENT(UTexture2D*,ButtonImage)
+	
+	SLATE_EVENT(FOnClicked, OnClicked)
+ 
+	SLATE_END_ARGS()
+ 
+	void Construct( const FArguments& InArgs);
+
+	void SetText(FString& NewText) ;
+	void SetImage( UTexture2D* NewImage);
+
+	// helper function
+	TSharedPtr<FSlateBrush> Convert_UT2D_SlateBrush (UTexture2D* NewImage);
+ 
+private:
+	FString ButtonText;
+	//FSlateBrush ButtonImage ;
+	TSharedPtr<FSlateBrush>  ButtonImage ;
+
+	TSharedPtr<STextBlock> TextBlock;
+	TSharedPtr<SImage> ImageWidget;
+	
+
+	FOnClicked OnClicked;
+};
