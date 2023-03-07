f768f390eaf8f862b76860d38c05537d9b31aa2d
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-07 23:36:32 +0530

    Added  CooreButtonWidget  UMG   Class

---
+++ Source/CoreUI/UMG/Button/CoreButtonWidget.cpp
@@ -0,0 +1,41 @@
+﻿#include "CoreUI/UMG/Button/CoreButtonWidget.h"
+#include "CoreUI/Slate/LeafWidgets/Buttons/CoreButton.h"
+
+UCoreButton::UCoreButton(const FObjectInitializer& ObjectInitializer)
+				: Super(ObjectInitializer)
+{
+	// Set default values for properties
+	ButtonText = "Button";
+	ButtonImage = nullptr;
+}
+
+void UCoreButton::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	// Update the widget with the latest property values
+	if (ButtonWidget.IsValid())
+	{
+		ButtonWidget->SetText(ButtonText);
+		ButtonWidget->SetImage(ButtonImage);
+	}	// Update the widget with the latest property values
+	if (ButtonWidget.IsValid())
+	{
+		ButtonWidget->SetText(ButtonText);
+		ButtonWidget->SetImage(ButtonImage);
+	}
+}
+
+TSharedRef<SWidget> UCoreButton::RebuildWidget()
+{
+	ButtonWidget = SNew(SCoreButton)
+		.ButtonText(ButtonText) // Convert FText to FString using FromString() function
+		.ButtonImage(ButtonImage)
+		//.OnClicked(BIND_UOBJECT_DELEGATE(FOnClicked, OnButtonClicked));
+		.OnClicked(OnClicked);
+
+	
+
+	return ButtonWidget.ToSharedRef();
+	
+}
---
+++ Source/CoreUI/UMG/Button/CoreButtonWidget.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "CoreUI/Slate/LeafWidgets/Buttons/CoreButton.h"
+#include "CoreButtonWidget.generated.h"
+
+UCLASS()
+class COREUI_API UCoreButton : public UButton
+{
+	GENERATED_BODY()
+
+public:
+	UCoreButton(const FObjectInitializer& ObjectInitializer);
+	
+	//virtual void NativeConstruct() override;
+
+	virtual void SynchronizeProperties() override;
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyButton")
+	FString ButtonText;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyButton")
+	FName ButtonName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyButton")
+	UTexture2D* ButtonImage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyButton")
+	FLinearColor ButtonColor;
+
+	//UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnClicked OnClicked;
+
+private:
+	TSharedPtr<class SCoreButton> ButtonWidget;
+	
+};
