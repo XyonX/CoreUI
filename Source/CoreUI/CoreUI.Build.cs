751c17d0211a71a2d2e3df7e4d88e3df2c26c6e7
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-07 23:17:05 +0530

    Restructured  Folder Style

--- Source/CoreUI/CoreUI.Build.cs
+++ Source/CoreUI/CoreUI.Build.cs
@@ -26,7 +26,9 @@ public class CoreUI : ModuleRules
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"UMG", 
+				"Slate", 
+				"SlateCore"
 			}
 			);
 			
