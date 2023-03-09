550ce58bb3e9b174c46f20af6e253a2b6f456b18
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-10 00:38:09 +0530

    Added Debug  Item Slate

---
+++ Source/CoreUI/Slate/CompoundWidgets/Debug_Item.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Debug_Item.h"
+#include  "Widgets/SWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SDebug_Item::Construct(const FArguments& InArgs)
+{
+	VariableName  = InArgs._VarName;
+	VariableValue = InArgs._VarValue;
+	TSharedPtr<SHorizontalBox>DWD;
+	
+	ChildSlot
+	[
+		SNew(SBox)
+		.WidthOverride(10)
+		.HeightOverride(50)
+		.Padding(1)
+		
+		[
+			SAssignNew(RootHorizontalBox,SHorizontalBox)
+				.Tag(TEXT("RootBox"))
+		
+			+SHorizontalBox::Slot()
+				.HAlign(HAlign_Left)
+				.VAlign(VAlign_Center)
+				.AutoWidth()
+				.Padding(5.0f)
+				[
+					SNew(SOverlay)
+						+ SOverlay::Slot()
+						.HAlign(HAlign_Fill)
+						.VAlign(VAlign_Fill)
+							[
+								SNew(SImage)
+								.ColorAndOpacity(FLinearColor(1, 1, 1, 1))     
+								//.Image(backgroundImage)
+							]
+						+ SOverlay::Slot()
+						.HAlign(HAlign_Center)
+						.VAlign(VAlign_Center)
+							[
+								SNew(STextBlock)
+								.Text(VariableName)
+							]
+				]
+
+			+SHorizontalBox::Slot()
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Center)
+				.AutoWidth()
+				.Padding(5.0f)
+				[
+					SNew(STextBlock)
+					.Text(VariableValue)
+				]
+
+			]
+	];
+	
+}
+
+void SDebug_Item::SetName(FText in_Name)
+{
+	VariableName = in_Name;
+}
+
+void SDebug_Item::SetValue(FText in_Value)
+{
+	VariableValue = in_Value;
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/CoreUI/Slate/CompoundWidgets/Debug_Item.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class COREUI_API SDebug_Item : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SDebug_Item)
+		{}
+
+	SLATE_ARGUMENT(FText , VarName)
+	SLATE_ARGUMENT(FText  , VarValue)
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+
+
+	// FUNCTIONS
+	void SetName (FText in_Name );
+	void  SetValue  (FText in_Value);
+
+	template<typename T>
+	FORCEINLINE typename TSharedPtr<T>::ElementType& SAssign(TSharedPtr<T>& Ptr, const typename TSharedPtr<T>::ElementType& Widget)
+	{
+		return *Ptr = Widget;
+	}
+
+private:
+	
+	TSharedPtr<SHorizontalBox> RootHorizontalBox ;
+	FText VariableName ;
+	FText VariableValue ;
+	UTexture2D*BG  ;
+};
