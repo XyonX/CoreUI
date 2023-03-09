e23ff443c2e9440e40aaff39cf06f3a5bf0b9138
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-10 00:38:21 +0530

    Added Debug  Item UMG

---
+++ Source/CoreUI/UMG/Debug/Debug_Item_Widget.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Debug_Item_Widget.h"
+
+
+UDebug_Item_Widget::UDebug_Item_Widget(const FObjectInitializer& ObjectInitializer)  : Super(ObjectInitializer)
+{
+	
+}
+
+UDebug_Item_Widget::UDebug_Item_Widget( const FObjectInitializer& ObjectInitializer, FText Name, FText Value)  :  Super(ObjectInitializer)
+{
+	VariableName  =Name;
+	VariableValue = Value;
+}
+
+
+
+TSharedRef<SWidget> UDebug_Item_Widget::RebuildWidget()
+{
+	DebugContainerWidget =SNew(SDebug_Item)
+							.VarName(VariableName)
+							.VarValue(VariableValue);
+
+	return DebugContainerWidget.ToSharedRef();
+	
+}
+
+void UDebug_Item_Widget::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+	DebugContainerWidget.Reset();
+}
---
+++ Source/CoreUI/UMG/Debug/Debug_Item_Widget.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "CoreUI/Slate/CompoundWidgets/Debug_Item.h"
+#include "Debug_Item_Widget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREUI_API UDebug_Item_Widget : public UWidget
+{
+	GENERATED_BODY() 
+public:
+	UDebug_Item_Widget(const FObjectInitializer& ObjectInitializer);
+	UDebug_Item_Widget(const FObjectInitializer& ObjectInitializer,  FText Name , FText Value);
+
+protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+
+	TSharedPtr<SDebug_Item> DebugContainerWidget ;
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+	
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UMG")
+	FText  VariableName  ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UMG")
+	FText  VariableValue;
+};
