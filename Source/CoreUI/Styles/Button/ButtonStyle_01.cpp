f8f97c48ae3087d87b638b519c691377d31d22a0
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-04-04 13:01:24 +0530

    Added Button Style class

---
+++ Source/CoreUI/Styles/Button/ButtonStyle_01.cpp
@@ -0,0 +1,54 @@
+﻿#include "ButtonStyle_01.h"
+
+#include "Styling/SlateStyleRegistry.h"
+
+TSharedPtr<FSlateStyleSet> FCustomButtonStyle::StyleSet = nullptr;
+
+FCustomButtonStyle::FCustomButtonStyle()
+{
+	// Set up the default button appearance here
+	// Set the button's background color to red
+	//BackgroundColor = FSlateColor(FLinearColor::Red);
+}
+
+const FName FCustomButtonStyle::TypeName("FCustomButtonStyle");
+
+const FCustomButtonStyle& FCustomButtonStyle::GetDefault()
+{
+	static FCustomButtonStyle Default;
+	return Default;
+}
+
+void FCustomButtonStyle::Initialize()
+{
+	if (!StyleSet.IsValid())
+	{
+		StyleSet = MakeShared<FSlateStyleSet>(FCustomButtonStyle::TypeName);
+		StyleSet->SetContentRoot(FPaths::ProjectPluginsDir() / TEXT("YourPluginName") / TEXT("Resources"));
+
+		// Set up the button appearance here
+	}
+
+	FSlateStyleRegistry::RegisterSlateStyle(*StyleSet.Get());
+}
+
+void FCustomButtonStyle::Shutdown()
+{
+	FSlateStyleRegistry::UnRegisterSlateStyle(*StyleSet.Get());
+	ensure(StyleSet.IsUnique());
+	StyleSet.Reset();
+}
+
+
+void FCustomButtonStyle::ReloadTextures()
+{
+	FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
+}
+
+
+FCustomButtonStyle::~FCustomButtonStyle()
+{
+	
+}
+
+#undef IMAGE_BRUSH
---
+++ Source/CoreUI/Styles/Button/ButtonStyle_01.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Styling/SlateStyle.h"
+#include "Styling/SlateTypes.h"
+
+class COREUI_API FCustomButtonStyle : public FButtonStyle
+{
+public:
+	FCustomButtonStyle();
+
+	static const FName TypeName;
+	static const FCustomButtonStyle& GetDefault();
+	
+	static TSharedPtr<FSlateStyleSet> StyleSet;
+
+	static void Initialize();
+	static void Shutdown();
+	static void ReloadTextures();
+
+	virtual ~FCustomButtonStyle() ;
+	
+};
