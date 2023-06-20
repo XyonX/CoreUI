e2de6d186b69325e58b38a4dfa7a2ceda150299b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-20 13:02:23 +0530

    Added Building Panel Slate class

---
+++ Source/CoreUI/Slate/CompoundWidgets/SBottomBuildingsPanel.cpp
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SBottomBuildingsPanel.h"
+
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SBottomBuildingsPanel::Construct(const FArguments& InArgs)
+{
+	SizeboxSize  = InArgs._Size;
+	BGBrush=InArgs._BGSlateBrush;
+	ChildSlot
+	[
+		SNew(SBorder)
+		.BorderBackgroundColor(FLinearColor(0.0f, 0.0f, 0.0f, 0.5f))
+		.Padding(0.0f)
+		[
+			BuildUI()
+		]
+	];
+	
+}
+
+TSharedRef<SWidget> SBottomBuildingsPanel::BuildUI()
+{
+
+	// Create the size box
+	TSharedRef<SBox> SizeBox = SNew(SBox)
+
+	.HAlign(HAlign_Fill)
+	.VAlign(VAlign_Bottom)
+	.HeightOverride(SizeboxSize.X)
+		[
+			// Create the horizontal box
+		SNew(SHorizontalBox)
+			
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+			
+				SNew(SImage)
+				.Image(BGBrush.Get())
+			]
+		];
+
+	return SizeBox;
+}
+FVector2D SBottomBuildingsPanel::CalcualteWidgetSize()
+{
+/*	// Get the screen or window dimensions
+	FVector2D ScreenSize = FVector2D::ZeroVector;
+	FWidgetPath WidgetPath;
+	UGameViewportClient* GameViewportClient = GEngine->GameViewport;
+	if (GameViewportClient)
+	{
+		TSharedRef<SWidget> ViewportWidgetRef = GameViewportClient->GetGameViewportWidget();
+		if (FSlateApplication::Get().FindPathToWidget(ViewportWidgetRef, WidgetPath))
+		{
+			const FGeometry& ViewportGeometry = WidgetPath.GetLastGeometry();
+			ScreenSize = ViewportGeometry.GetAbsoluteSize();
+		}
+	}
+	*/
+
+	//Viewport Size
+	//FIntPoint GViewport = FIntPoint(GEngine->GameViewport->Viewport->GetSizeXY());
+	//FVector2D MyVector(static_cast<FVector2D>(GViewport));
+	//const FVector2D ViewportSize ((FVector2D)GViewport);
+
+	////Viewport Center!
+	//const FVector2D ViewportCenter = FVector2D(ViewportSize.X/2, ViewportSize.Y/2);
+return FVector2D::ZeroVector;
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/CoreUI/Slate/CompoundWidgets/SBottomBuildingsPanel.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class COREUI_API SBottomBuildingsPanel : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SBottomBuildingsPanel)
+	: _Size(0,200)
+	{}
+
+	SLATE_ARGUMENT(FVector2D , Size)
+	SLATE_ARGUMENT(TSharedPtr<FSlateBrush> ,BGSlateBrush)
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+
+	TSharedRef<SWidget> BuildUI();
+	FVector2D CalcualteWidgetSize ();
+
+	FVector2D SizeboxSize ;
+	TSharedPtr<FSlateBrush> BGBrush ;
+	TSharedPtr<UTexture2D> BGImage ;
+};
