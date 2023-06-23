a63cc669336f960e5c3e92a4b23d7fbde8bc35c5
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-23 22:05:59 +0530

    Added Test UMG Class

---
+++ Source/CoreUI/UI/TestUMG.cpp
@@ -0,0 +1,37 @@
+﻿#include "TestUMG.h"
+
+#include "CoreUI/Slate/Cards/SBuildingCard.h"
+
+UTestUMG::UTestUMG(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+	
+}
+
+void UTestUMG::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	if(BuildingCard.IsValid())
+	{
+		BuildingCard->SetBrush(BuildingImage);
+	}
+}
+
+TSharedRef<SWidget> UTestUMG::RebuildWidget()
+{
+
+	SAssignNew(  BuildingCard,SBuildingCard);
+	return BuildingCard.ToSharedRef();
+}
+
+void UTestUMG::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+	BuildingCard.Reset();
+}
+
+void UTestUMG::SetBuildingImage(UTexture2D* Texture)
+{
+	BuildingImage=Texture;
+}
---
+++ Source/CoreUI/UI/TestUMG.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TestUMG.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREUI_API UTestUMG : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UTestUMG(const FObjectInitializer& ObjectInitializer);
+	
+	//virtual void NativeConstruct() override;
+
+	virtual void SynchronizeProperties() override;
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+
+
+
+	void SetBuildingImage (UTexture2D* Texture);
+
+
+
+private:
+	TSharedPtr<class SBuildingCard>BuildingCard;
+	
+	UTexture2D*BuildingImage;
+
+	
+};
