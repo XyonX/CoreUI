c6e3bde6a996255063202a898fb1b073e556228f
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-23 14:05:58 +0530

    Added Building Card UI

---
+++ Source/CoreUI/Slate/Cards/SBuildingCard.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SBuildingCard.h"
+
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SBuildingCard::Construct(const FArguments& InArgs)
+{
+	
+	ChildSlot
+	[
+         SNew(SOverlay)
+         +SOverlay::Slot()
+         .HAlign(HAlign_Center)
+         .VAlign(VAlign_Center)
+         [
+				SNew(SBox)
+				.HeightOverride(200)
+				.WidthOverride(200)
+				[
+					SNew(SImage)
+					.Image(InArgs._inBrush1.Get())
+				]
+         ]
+         
+	];
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/CoreUI/Slate/Cards/SBuildingCard.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class COREUI_API SBuildingCard : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SBuildingCard)
+		{
+		}
+	SLATE_ARGUMENT(TSharedPtr<FSlateBrush> , inBrush1)
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+public:
+
+	TSharedPtr<FSlateBrush>Brush1;
+};
